ffdde6e2517ed849cf00977b81969dfc8d00b158
Gagan <141204441+GaganHonor@users.noreply.github.com>
2023-09-23 17:14:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,33 +6,32 @@ from telegram import ChatAction
 import time
 
 BOT_TOKENS = [
-    "6253669210:AAGiUM6kodf9CPuIEXnCpkJ6geJRuwpRQhw"
-    "6337536275:AAFZ2PAb0lni9xZlHPIiloWpqarFZ8L4bjY"
+    "6253669210:AAGiUM6kodf9CPuIEXnCpkJ6geJRuwpRQhw",
+    "6337536275:AAFZ2PAb0lni9xZlHPIiloWpqarFZ8L4bjY",
 ]
-
 
 
 def start_command(update: Update, context: CallbackContext):
     user_id = update.message.chat_id
     first_name = update.message.from_user.first_name
     last_name = update.message.from_user.last_name
-        
-        # Show typing animation
+
+    # Show typing animation
     context.bot.send_chat_action(chat_id=user_id, action=ChatAction.TYPING)
     time.sleep(2)  # Simulate loading time
 
     context.bot.send_message(
-    chat_id=user_id,
-    text=f" Sorry, {first_name} for the inconvenience! Global maintenance is in progress... 🪲 \nTry again later..  \n ~ Team AOC ( DEVS ) "
-        )
+        chat_id=user_id,
+        text=f"Sorry, {first_name} for the inconvenience! Global maintenance is in progress... 🪲\nTry again later..\n~ Team AOC (DEVS)",
+    )
 
-        # Send the image along with the message
+    # Send the image along with the message
     context.bot.send_chat_action(chat_id=user_id, action=ChatAction.UPLOAD_PHOTO)
     time.sleep(2)  # Simulate loading time
     context.bot.send_photo(
-    chat_id=user_id,
-    photo=open("/home/admin/notice/New_Noticebot/image.jpeg", "rb")
-        )
+        chat_id=user_id,
+        photo=open("/home/admin/notice/New_Noticebot/image.jpeg", "rb"),
+    )
 
     context.user_data["last_command"] = "start"
 
@@ -42,19 +41,13 @@ def main():
         bot = telegram.Bot(token=token)
         updater = Updater(bot.token, use_context=True)
         dispatcher = updater.dispatcher
-        start_handler = CommandHandler("start", start_command)
-        set_value_handler = CommandHandler("setvalue", set_value_command)
-def main():
-    for token in BOT_TOKENS:
-        bot = telegram.Bot(token=token)
-        updater = Updater(bot.token, use_context=True)
-        dispatcher = updater.dispatcher
-        start_handler = CommandHandler("start", start_command)
-       
-        dispatcher.add_handler(start_handler)
-        
+
+        # Add the start command handler to all bots
+        dispatcher.add_handler(CommandHandler("start", start_command))
+
         updater.start_polling()
-    updater.idle()
+        updater.idle()
+
 
 if __name__ == "__main__":
     main()
