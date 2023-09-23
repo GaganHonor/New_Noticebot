82d7524e156fb5742923cb321316757b4d7b07f7
Gagan <141204441+GaganHonor@users.noreply.github.com>
2023-09-23 17:19:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import telegram
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
@@ -10,11 +11,27 @@ BOT_TOKENS = [
     "6337536275:AAFZ2PAb0lni9xZlHPIiloWpqarFZ8L4bjY",
 ]
 
+logging.basicConfig(level=logging.DEBUG)
+
+
+def is_user_blocked(bot: telegram.Bot, user_id: int) -> bool:
+    try:
+        bot.get_chat(chat_id=user_id)
+        return False
+    except telegram.error.Unauthorized as e:
+        logging.error(e)
+        return True
+
 
 def start_command(update: Update, context: CallbackContext):
     user_id = update.message.chat_id
     first_name = update.message.from_user.first_name
     last_name = update.message.from_user.last_name
+
+    # Check if the user has blocked the bot
+    if is_user_blocked(context.bot, user_id):
+        logging.info(f"User {user_id} has blocked the bot")
+        return
 
     # Show typing animation
     context.bot.send_chat_action(chat_id=user_id, action=ChatAction.TYPING)
