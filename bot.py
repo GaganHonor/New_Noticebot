1d291a05160448f7cea5720390047ca5546823fc
Gagan <141204441+GaganHonor@users.noreply.github.com>
2023-09-23 10:37:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ def start_command(update: Update, context: CallbackContext):
         else:
             context.bot.send_message(
                 chat_id=user_id,
-                text=f"<b> Sorry, {first_name} for the inconvenience! This bot is on Global maintenance services 🤖 \n We will come back soon.. </b>"
+                text=f" Sorry, {first_name} for the inconvenience! This bot is on Global maintenance services 🤖 \n We will come back soon.. "
             )
             context.bot.forward_message(chat_id=ADMIN_ID, from_chat_id=user_id, message_id=update.message.message_id)
     else:
@@ -42,7 +42,7 @@ def start_command(update: Update, context: CallbackContext):
 
         context.bot.send_message(
             chat_id=user_id,
-            text=f"<b> Sorry, {first_name} for the inconvenience! This bot is on Global maintenance services 🤖 \n We will come back soon.. </b>"
+            text=f" Sorry, {first_name} for the inconvenience! This bot is on Global maintenance services 🤖 \n We will come back soon.. "
         )
 
         # Send the image along with the message
@@ -50,55 +50,11 @@ def start_command(update: Update, context: CallbackContext):
         time.sleep(2)  # Simulate loading time
         context.bot.send_photo(
             chat_id=user_id,
-            photo=open("image.jpeg", "rb")
+            photo=open("/home/admin/notice/New_Noticebot/image.jpeg", "rb")
         )
 
     context.user_data["last_command"] = "start"
 
-def set_value_command(update: Update, context: CallbackContext):
-    user_id = update.message.chat_id
-    if user_id == ADMIN_ID:
-        text = update.message.text
-        input_value = text.split()[1]
-
-        # Update the value of the variable
-        context.user_data["counter"] = int(input_value)
-
-        context.bot.send_message(
-            chat_id=user_id,
-            text=f"The value has been updated to {input_value}."
-        )
-    else:
-        context.bot.send_message(
-            chat_id=user_id,
-            text="You are not authorized to use this command."
-        )
-
-def restart_command(update: Update, context: CallbackContext):
-    user_id = update.message.chat_id
-    if user_id == ADMIN_ID:
-        context.bot.send_message(
-            chat_id=user_id,
-            text="Restarting the bot..."
-        )
-
-        os.execl(sys.executable, sys.executable, *sys.argv)
-    else:
-        context.bot.send_message(
-            chat_id=user_id,
-            text="You are not authorized to use this command."
-        )
-
-def forward_message_to_user(update: Update, context: CallbackContext):
-    user_id = ADMIN_ID
-    message = update.message.reply_to_message
-    forward_from_id = message.forward_from.id
-    forward_text = message.text
-
-    context.bot.send_message(
-        chat_id=forward_from_id,
-        text=f"Admin message: {forward_text}"
-    )
 
 def main():
     for token in BOT_TOKENS:
@@ -113,13 +69,9 @@ def main():
         updater = Updater(bot.token, use_context=True)
         dispatcher = updater.dispatcher
         start_handler = CommandHandler("start", start_command)
-        set_value_handler = CommandHandler("setvalue", set_value_command)
-        restart_handler = CommandHandler("restart", restart_command)
-        forward_handler = MessageHandler(Filters.reply & Filters.user(ADMIN_ID), forward_message_to_user)
+       
         dispatcher.add_handler(start_handler)
-        dispatcher.add_handler(set_value_handler)
-        dispatcher.add_handler(restart_handler)
-        dispatcher.add_handler(forward_handler)
+        
         updater.start_polling()
     updater.idle()
 
